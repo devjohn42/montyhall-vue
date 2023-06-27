e8aef69213d632c84042714b6883742dc6b02037
João Vitor <devjohncb@gmail.com>
2023-06-26 22:39:16 -0300

    Gift componentn created

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-
+  <div>
+    <h1>Problema de Monty Hall</h1>
+    <Gift />
+  </div>
 </template>
 
 <script>
-export default {
+import Gift from "./components/Gift.vue";
 
+export default {
+  name: "App", //ajuda na depuração caso apresenta algum erro(ñ obrigatório);
+  components: { Gift },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
